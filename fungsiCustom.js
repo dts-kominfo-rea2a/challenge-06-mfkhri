0c8cd6798410c109511cac007de71f11df913104
muhammad fakhri <12muhammadfakhri@gmail.com>
2022-06-12 08:16:00 +0700

    feat: push tugas ke-6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,37 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  // console.log(fnCallBack);
+  let result = [];
+
+  // json 1
+  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
+    // console.log(data);
+    if (err) {
+      return fnCallBack(err, null);
+    }
+    const resultData = JSON.parse(data);
+    result.push(resultData.message.split(" ")[1]);
+    // json 2
+    fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
+      if (err) {
+        return fnCallBack(err, null);
+      }
+      const resultData = JSON.parse(data);
+      result.push(resultData[0].message.split(" ")[1]);
+      // json 3
+      fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
+        if (err) {
+          return fnCallBack(err, null);
+        }
+        const resultData = JSON.parse(data);
+        result.push(resultData[0].data.message.split(" ")[1]);
+        fnCallBack(null, result);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
